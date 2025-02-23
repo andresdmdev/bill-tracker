b3bc5887d1cd37e0f0ba7266bb2cfbb5be52770c
Andres Marquez <amarquez@tiendapago.com>
2025-02-23 12:40:09 -0500

    Implement photo handling service with Telegram and Google Drive integration

--- services/handlers.py
+++ services/handlers.py
@@ -1,5 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import ContextTypes
+
+from services.photoService import PhotoService
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me! - Hosted in AWS Lambda")
@@ -8,7 +11,28 @@ async def echo_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await context.bot.send_message(chat_id=update.effective_chat.id, text="Not implemented")
 
 async def echo_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await context.bot.send_message(chat_id=update.effective_chat.id, text="Not implemented")
+  photo_service = PhotoService()
+  photo_service.save_photo(update.message.photo[len(update.message.photo) - 1].file_id, "photoBase64.txt", "photo.jpg")
+  print(f"Photo saved to photo.jpg")
+
+  folder_id = os.getenv('GOOGLE_DRIVE_FOLDER_ID')
+
+  file_id = await photo_service.save_photo_to_google_drive(
+    photo_service.photo_base64, 
+    folder_id,
+    filename="photo.jpg", 
+    is_base64=True
+  )
+  print(f"Photo saved to Google Drive with ID: {file_id}")
+
+  ## Obtener la imagen en base64 -- Listo
+  ## Guardar en google drive
+  ## Analizar con AI
+  ## Obtener los valores que vamos a guardar en dbfile_id
+  ## Guardar en base de datos
+  ## Enviar notificacion por correo - opcional
+  ## Devolver respuesta al usuario
+  await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Photo saved to Google Drive with ID: {file_id}")
 
 async def echo_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await context.bot.send_message(chat_id=update.effective_chat.id, text="Not implemented")
---
+++ services/photoService.py
@@ -0,0 +1,80 @@
+import json
+import os, requests, base64
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+from io import BytesIO
+
+class PhotoService:
+    def __init__(self):
+        self.token = os.getenv('TELEGRAM_TOKEN')
+        self.base_url_telegram = f"https://api.telegram.org/bot{self.token}"
+        self.file_url_telegram = f"https://api.telegram.org/file/bot{self.token}"
+        self.photo_base64 = ""
+        self.photo_data = ""
+
+    def get_file_path_from_telegram(self, file_id):
+        url = f"{self.base_url_telegram}/getFile"
+        response = requests.get(url, params={'file_id': file_id})
+        return response.json()['result']['file_path']
+
+    def download_photo_from_telegram(self, file_path):
+        url = f"{self.file_url_telegram}/{file_path}"
+        response = requests.get(url)
+        return response.content
+
+    def save_photo(self, file_id, save_path_base64, save_path_image):
+        file_path = self.get_file_path_from_telegram(file_id)
+        self.photo_data = self.download_photo_from_telegram(file_path)
+
+        self.photo_base64 = base64.b64encode(self.photo_data).decode('utf-8')
+        with open(save_path_base64, 'w') as f:
+            f.write(self.photo_base64)
+
+        with open(save_path_image, 'wb') as f:
+            f.write(self.photo_data)
+
+    async def save_photo_to_google_drive(self, photo_data, folder_id, filename="photo.jpg", is_base64=True):
+        try:
+            credentials_json = json.loads(os.environ.get('GOOGLE_CREDENTIALS'))
+            credentials = service_account.Credentials.from_service_account_info(
+                credentials_json,
+                scopes=['https://www.googleapis.com/auth/drive.file']
+            )
+            
+            service = build('drive', 'v3', credentials=credentials)
+            
+            # Preparar los datos según el formato
+            if is_base64:
+                photo_bytes = base64.b64decode(photo_data)
+            else:
+                photo_bytes = photo_data
+            
+            # Crear archivo temporal en memoria
+            file_buffer = BytesIO(photo_bytes)
+            
+            # Agregar el ID de la carpeta donde se guardará
+            file_metadata = {
+                'name': filename,
+                'parents': [folder_id]
+            }
+            
+            # Usar MediaIoBaseUpload en lugar de MediaFileUpload
+            media = MediaIoBaseUpload(
+                file_buffer,
+                mimetype='image/jpeg',
+                resumable=True
+            )
+            
+            file = service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields='id'
+            ).execute()
+            
+            return file.get('id')
+        
+        except Exception as e:
+            print(f"Error detallado: {str(e)}")
+            raise Exception(f"Error al subir a Google Drive: {str(e)}")
+
